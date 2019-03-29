9493f01da7a807a843badd56d04b7b27c8bbd307
Wklee96 <lweikang96@gmail.com>
2019-03-29 14:38:35 +0800

    done search feature for user job requests

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -20,6 +20,7 @@ const pool = new Pool({
 /* SQL Query */
 var sql_query_request = sql_query.query.query_request_user;
 var sql_query_offer = sql_query.query.query_offer_user;
+var sql_query_search_request = sql_query.query.query_request_search;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -36,6 +37,21 @@ router.get('/', function(req, res, next) {
 	} else {
 		res.render('signuplogin', { title: 'login'});
 	}
+});
+
+/* POST for search */
+router.post('/', function(req, res) {
+	pool.query(sql_query_search_request, ['%' + req.body.task_search + '%', req.user.username], (err, search) => {
+		if (!err) {
+			pool.query(sql_query_offer, [req.user.username], (err, offers) => {
+				if (!err) {
+					res.render('dashboard', { title: 'dashboard', requests: search.rows, offers: offers.rows });
+				}
+			})
+		} else {
+			console.log(err)
+		}
+	});
 });
 
 module.exports = router;
