0041b426d14f54a13d15f4fd2b7db8377ee0c753
Wklee96 <lweikang96@gmail.com>
2019-04-01 09:36:40 +0800

    fix accept button

--- sql/index.js
+++ sql/index.js
@@ -16,9 +16,18 @@ sql.query = {
 	query_request_user: 'SELECT * FROM job_request WHERE job_request.user=$1',
 	query_offer_user: 'SELECT * FROM job_offer WHERE job_offer.user=$1',
 
+	// Query tasks on job id
+	query_request_job: 'SELECT * FROM job_request WHERE job_request.job_id=$1',
+	query_offer_job: 'SELECT * FROM job_offer WHERE job_offer.job_id=$1',
+	query_bids_job: 'SELECT * from request_bids WHERE job_id=$1',
+
 	// Query tasks on task name
 	query_request_search: 'SELECT * FROM job_request WHERE LOWER(job_request.job) LIKE LOWER($1) and job_request.user=$2',
-	query_offer_search: 'SELECT * FROM job_offer WHERE LOWER(job_offer.job) LIKE LOWER($1) and job_offer.user=$2'
+	query_offer_search: 'SELECT * FROM job_offer WHERE LOWER(job_offer.job) LIKE LOWER($1) and job_offer.user=$2',
+
+	// Insert bids
+	insert_request_bids: 'INSERT INTO request_bids VALUES($1, $2, $3, $4)',
+	update_request_bids: 'UPDATE request_bids as b SET bid_accepted=true where bid_id = $1 and exists (select 1 from job_request as j where j.job_id = b.job_id and j.user = $2)'
 }
 
 module.exports = sql
