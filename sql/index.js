c19d721cdd0f5896edffc1792ad01082ee5b10fe
Wklee96 <lweikang96@gmail.com>
2019-04-04 15:20:26 +0800

    updated dashboard

--- sql/index.js
+++ sql/index.js
@@ -17,14 +17,15 @@ sql.query = {
 	// Query tasks on user id
 	query_request_user: 'SELECT * FROM job_request WHERE job_request.user=$1 AND NOT EXISTS (SELECT 1 FROM request_in_progress WHERE job_id=job_request.job_id)',
 	query_offer_user: 'SELECT * FROM job_offer WHERE job_offer.user=$1 AND NOT EXISTS (SELECT 1 FROM offer_in_progress WHERE job_id=job_offer.job_id)',
+	query_request_inprog: 'SELECT * FROM job_request WHERE job_request.user=$1 AND EXISTS (SELECT 1 FROM request_in_progress WHERE job_id=job_request.job_id)',
+	query_offer_inprog: 'SELECT * FROM job_offer WHERE job_offer.user=$1 AND EXISTS (SELECT 1 FROM offer_in_progress WHERE job_id=job_offer.job_id)',
 
 	// Query tasks on job id
 	query_request_job: 'SELECT * FROM job_request WHERE job_request.job_id=$1',
 	query_offer_job: 'SELECT * FROM job_offer WHERE job_offer.job_id=$1',
 	query_bids_request: 'SELECT * FROM request_bids WHERE job_id=$1',
 	query_bids_offer: 'SELECT * FROM offer_bids WHERE job_id=$1',
-	query_request_inprog: 'SELECT * FROM job_request WHERE job_request.job_id=$1 AND EXISTS (SELECT 1 FROM request_in_progress WHERE job_id=$1)',
-	query_offer_inprog: 'SELECT * FROM job_offer WHERE job_offer.job_id=$1 AND EXISTS (SELECT 1 FROM offer_in_progress WHERE job_id=$1)',
+
 
 	// Query tasks on task name
 	query_request_search: 'SELECT * FROM job_request WHERE LOWER(job_request.job) LIKE LOWER($1) and job_request.user=$2',
