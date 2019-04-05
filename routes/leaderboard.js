a68d358c226c95c02ed2503a7afdacc91a7fee24
skpai27 <skpai27@yahoo.com.sg>
2019-04-06 00:59:51 +0800

    leaderboard top completers part done

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -10,15 +10,18 @@ const pool = new Pool({
 
 /* SQL Query */
 var sql_query_request_top_offerers = sql_query.query.query_request_top_offerers;
+var sql_query_request_top_completers = sql_query.query.query_request_top_completers;
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	if (req.isAuthenticated()) {
-		pool.query(sql_query_request_top_offerers, (err, requests) => {
-			if (!err) {
-                res.render('leaderboard', { title: 'leaderboard', requests: requests.rows});
-            }
+		pool.query(sql_query_request_top_offerers, (err, top_offerers) => {
+            pool.query(sql_query_request_top_completers, (err1, top_completers) => {
+                if (!err) {
+                    res.render('leaderboard', { title: 'leaderboard', top_offerers: top_offerers.rows, top_completers: top_completers.rows});
+                }
+            })
 		});
 	} else {
 		res.render('signuplogin', { title: 'login'});
